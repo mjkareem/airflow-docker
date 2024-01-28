611e1be9990c9185a603d5d78d9e06660aed6a49
mjkareem <mujahidakande@gmail.com>
2024-01-28 06:22:34 +0100

    Your commit message

--- dags/our_first_dag.py
+++ dags/our_first_dag.py
@@ -5,7 +5,7 @@ from airflow.operators.bash import BashOperator
 
 
 default_args = {
-    'owner': 'coder2j',
+    'owner': 'mjkareem',
     'retries': 5,
     'retry_delay': timedelta(minutes=2)
 }
@@ -15,9 +15,9 @@ with DAG(
     dag_id='our_first_dag_v5',
     default_args=default_args,
     description='This is our first dag that we write',
-    start_date=datetime(2021, 7, 29, 2),
+    start_date=datetime(2024, 1, 27, 2),
     schedule_interval='@daily'
-) as dag:
+) as dagm:
     task1 = BashOperator(
         task_id='first_task',
         bash_command="echo hello world, this is the first task!"
